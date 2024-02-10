68427b89992db6e7bf0e00ecfd2a93eaf5c8ebc7
Devang Vishnu <thedevangvishnu@gmail.com>
2024-02-10 17:31:56 +0530

    Make the DetailsBanner comp responsive

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -2,9 +2,12 @@ import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 import DetailsBanner from "./detailsBanner/DetailsBanner";
+import useFetch from "../../hooks/useFetch";
 
 const Details = () => {
   const { mediaType, id } = useParams();
+
+  const { data, isLoading } = useFetch(`/${mediaType}/${id}/credits`);
 
   useEffect(() => {
     console.log(mediaType);
@@ -13,7 +16,7 @@ const Details = () => {
 
   return (
     <div>
-      <DetailsBanner mediaType={mediaType} id={id} />
+      <DetailsBanner mediaType={mediaType} id={id} crew={data?.crew} />
     </div>
   );
 };
--- src/pages/details/detailsBanner/DetailsBanner.jsx
+++ src/pages/details/detailsBanner/DetailsBanner.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import dayjs from "dayjs";
 
 import "./DetailsBanner.styles.scss";
@@ -9,7 +9,20 @@ import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
 import Genres from "../../../components/genres/Genres";
 import CircleRating from "../../../components/circleRating/CircleRating";
 
-const DetailsBanner = ({ mediaType, id }) => {
+const DetailsBanner = ({ mediaType, id, crew }) => {
+  const directors = crew?.filter(
+    (crewMember) =>
+      crewMember.job === "Director" ||
+      crewMember.known_for_department === "Directing"
+  );
+  const writers = crew?.filter(
+    (crewMember) =>
+      crewMember.job === "Writer" ||
+      crewMember.job === "Screenplay" ||
+      crewMember.job === "Story" ||
+      crewMember.known_for_department === "Writing"
+  );
+
   const BASE_URL = "https://image.tmdb.org/t/p/original";
 
   const { data, isLoading } = useFetch(`/${mediaType}/${id}`);
@@ -17,11 +30,21 @@ const DetailsBanner = ({ mediaType, id }) => {
   const backdropUrl = `${BASE_URL}${data?.backdrop_path}`;
   const posterUrl = `${BASE_URL}${data?.poster_path}`;
 
+  const formatDuration = (duration) => {
+    const hours = (duration / 60).toFixed(0);
+    const minutes = duration % 60;
+
+    const totalDuration = `${hours}hrs ${minutes}mins`;
+    return totalDuration;
+  };
+
   return (
     <div className="detailsBanner">
       <div className="detailsBackdropContainer">
         <LazyImage src={`${backdropUrl}`} />
       </div>
+
+      <div className="overlayContainer"></div>
 
       <ContentWrapper>
         {/* left section */}
@@ -54,6 +77,74 @@ const DetailsBanner = ({ mediaType, id }) => {
           {/* rating */}
           <div className="ratingContainer">
             <CircleRating rating={data?.vote_average?.toFixed(1)} />
+          </div>
+
+          {/* overview */}
+          <div className="overviewContainer">
+            <p className="overviewHeading">Overview</p>
+            <p className="overviewContent">{data?.overview}</p>
+          </div>
+
+          {/* metaData */}
+          <div className="metaDataContainer">
+            <div className="metaDataItems">
+              {/* status, release data and duration */}
+              <div className="metaDataItem">
+                <div className="subItem">
+                  <p className="tag">Status:</p>
+                  <p className="value">{data?.status}</p>
+                </div>
+
+                <div className="subItem">
+                  <p className="tag">Release:</p>
+                  <p className="value">
+                    {dayjs(data?.release_date).format("MMM D, YYYY")}
+                  </p>
+                </div>
+
+                <div className="subItem">
+                  <p className="tag">Duration:</p>
+                  <p className="value">{formatDuration(data?.runtime)}</p>
+                </div>
+              </div>
+
+              {/* directors */}
+              <div className="metaDataItem">
+                <div className="subItem crew">
+                  <p className="tag">Directors:</p>
+
+                  <p className="value">
+                    {directors?.map((director, index) =>
+                      directors.length <= 0
+                        ? "Not available"
+                        : directors.length === 1
+                        ? director?.name
+                        : index === directors.length - 1
+                        ? director?.name
+                        : director?.name + ", "
+                    )}
+                  </p>
+                </div>
+              </div>
+
+              {/* writers */}
+              <div className="metaDataItem">
+                <div className="subItem crew">
+                  <p className="tag">Writers:</p>
+                  <p className="value">
+                    {writers?.map((writer, index) =>
+                      writers.length < 1
+                        ? "Not available"
+                        : writers.length === 1
+                        ? writer?.name
+                        : index === writers.length - 1
+                        ? writer?.name
+                        : writer?.name + ", "
+                    )}
+                  </p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </ContentWrapper>
