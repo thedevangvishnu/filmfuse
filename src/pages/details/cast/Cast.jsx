173f67263a069c9bf3eb4adb97dbb7dff38c0cbc
Devang Vishnu <thedevangvishnu@gmail.com>
2024-02-12 10:33:29 +0530

    Add poster fallback images

--- src/pages/details/cast/Cast.jsx
+++ src/pages/details/cast/Cast.jsx
@@ -3,6 +3,7 @@ import React, { useEffect } from "react";
 import "./Cast.styles.scss";
 import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
 import LazyImage from "../../../components/lazyLoadImage/LazyImage";
+import FallbackPoster from "../../../assets/images/avatar.png";
 
 const Cast = ({ cast, castLoading }) => {
   const castListSorted = cast?.sort((a, b) => a.popularity > b.popularity);
@@ -19,7 +20,13 @@ const Cast = ({ cast, castLoading }) => {
               return (
                 <div className="castItem" key={member.id}>
                   <div className="pictureContainer">
-                    <LazyImage src={`${BASE_URL}/${member?.profile_path}`} />
+                    <LazyImage
+                      src={
+                        !member?.profile_path
+                          ? FallbackPoster
+                          : `${BASE_URL}/${member?.profile_path}`
+                      }
+                    />
                   </div>
                   <p className="name">{member?.name}</p>
                   <p className="character">{member?.character}</p>
